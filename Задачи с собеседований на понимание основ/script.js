77a53609c0bb786eb84ea33be13030def2b41f93
Timur Zhalgasbaev <timur_ti-tim@mail.ru>
2020-10-09 16:34:59 +0500

    Задачи с собеседований на понимание основ

---
+++ Задачи с собеседований на понимание основ/script.js
@@ -0,0 +1,32 @@
+'use strict';
+
+// let x = 5; alert( x++ );
+
+// [ ] + false - null + true
+// console.log([] + false - null + true); // "NaN"
+
+// let y = 1; 
+// let x = y = 2;  
+// alert(x);
+
+// console.log([ ] + 1 + 2);
+
+// alert( "1"[0] );
+
+// console.log(2 && 1 && null && 0 && undefined);
+// И запинается на лжи
+
+// console.log(!!( 1 && 2 ) === (1 && 2));
+
+// alert( null || 2 && 3 || 4 );
+
+// const a = [1, 2, 3]; 
+// const b = [1, 2, 3]; 
+
+// console.log(a == b);
+
+// alert( +"Infinity" );
+
+// console.log("Ёжик" > "яблоко");
+
+// console.log(0 || "" || 2 || undefined || true || falsе);
