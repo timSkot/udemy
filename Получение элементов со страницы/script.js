70e600b348672edb1e10ed096235694043cf094e
Timur Zhalgasbaev <timur_ti-tim@mail.ru>
2020-10-12 18:06:47 +0500

    Практика. Задание на отработку действий со страницей

--- Получение элементов со страницы/script.js
+++ Получение элементов со страницы/script.js
@@ -1,18 +1,43 @@
 'use strict';
 
-const box = document.getElementById('box');
-console.log(box);
+const box = document.getElementById('box'),
+      btns = document.getElementsByTagName('button'),
+      circles = document.getElementsByClassName('circle'),
+      wrapper = document.querySelector('.wrapper'),
+      hearts = wrapper.querySelectorAll('.heart'),
+      oneHeart = wrapper.querySelector('.heart');
 
-const btns = document.getElementsByTagName('button');
-console.log(btns[0]);
+// box.style.backgroundColor = 'blue';
+// box.style.width = '500px';
 
-const circles = document.getElementsByClassName('circle');
-console.log(circles);
+box.style.cssText = 'background-color: orange; width: 500px';
 
-const hearts = document.querySelectorAll('.heart');
+btns[1].style.borderRadius = '100%';
+circles[0].style.backgroundColor = 'red';
+
+// for (let i = 0; i < hearts.length; i++) {
+//     hearts[i].style.backgroundColor = 'blue';
+// }
+
 hearts.forEach(item => {
-    console.log(item);
+    item.style.backgroundColor = 'green';
 });
 
-const oneHeart = document.querySelector('.heart');
-console.log(oneHeart);
+const div = document.createElement('div');
+// const text = document.createTextNode('Тут был я');
+
+div.classList.add('black');
+
+wrapper.append(div);
+// wrapper.appendChild(div);
+// wrapper.prepend(div);
+// hearts[0].before(div);
+// hearts[0].after(div);
+// wrapper.insertBefore(div, hearts[2]);
+// circles[0].remove();
+// wrapper.removeChild(hearts[1]);
+hearts[0].replaceWith(circles[0]);
+// wrapper.replaceChild(circles[0], hearts[0]);
+div.innerHTML = '<h1>Hello World</h1>';
+// div.textContent = '<h1>Hello World</h1>';
+div.insertAdjacentHTML('afterend', '<h2>Hello</h2>');
