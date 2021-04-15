ecccdc69e4439fc4be57333d86824825363b6df5
timskot <timur_ti-tim@mail.ru>
2021-04-15 19:38:44 +0500

    Работа с датами

---
+++ Работа с датами/main.js
@@ -0,0 +1,22 @@
+'use strict'
+
+// const now = new Date('2020-05-01');
+//  new Date.parse('2020-05-01');
+//
+//
+// console.log(now.getFullYear());
+// console.log(now.getMonth());
+// console.log(now.getDate());
+// console.log(now.getHours());
+// console.log(now.getUTCHours());
+// console.log(now.getTimezoneOffset());
+
+let start = new Date();
+
+for (let i = 0; i < 100000; i++) {
+     let some = i ** 3;
+}
+
+let end = new Date();
+
+alert(`Цикл отработал за ${end - start} миллисекунду`);
